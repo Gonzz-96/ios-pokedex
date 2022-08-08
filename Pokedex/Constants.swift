55ed357d7e38707baff2f9df98fd98607cc7834a
Gonzalo Campos <gonzaloc@spotify.com>
2022-08-08 12:08:33 +0100

    Set dummy data for table cell.

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Erick Gonzalo Campos on 08/08/22.
+//
+
+import Foundation
+
+class Constants {
+    static let pokemonCellIdentifier = "pokemonCell"
+}
--- Pokedex/PokedexList/PokemonListView.swift
+++ Pokedex/PokedexList/PokemonListView.swift
@@ -12,7 +12,35 @@ class PokemonListViewController: UIViewController, PokemonListView {
     
     var presenter: PokemonListPresenter?
     
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self,
+                       forCellReuseIdentifier: Constants.pokemonCellIdentifier)
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.frame = view.safeAreaLayoutGuide.layoutFrame
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension PokemonListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: Constants.pokemonCellIdentifier,
+            for: indexPath
+        )
+        cell.textLabel?.text = "Random text"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
     }
 }
